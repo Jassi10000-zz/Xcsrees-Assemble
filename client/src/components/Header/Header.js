6d98cf5b9374ad3b8d5b0b2b87acedf88bb2106e
Jaskeerat Singh <jaskeerat10000@gmail.com>
2021-10-10 20:52:17 +0530

    'integrated tailwindcss ⭐

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+           <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+                <div>
+                    <div class="text-xl font-medium text-black">Xcrees-Assemble</div>
+                    <p class="text-gray-500">Shop all the accessories at this one stop shop</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
